19298329de72b10f9afdaa63b404a38068b6bf99
Tiff <kim.tiffany.min@gmail.com>
2015-08-08 18:39:43 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chowscout::Application.config.secret_key_base = '4d0aad4b6c9e678fe33061f27a869d0c7efa669e32490a42bd1db6258910d4df61a5f159e05d63a521876c745657f9074358eb6913abf0ab1740f36266287ad3'
