177c9846e824ef3123565e53db19d831358a030a
Tiff <kim.tiffany.min@gmail.com>
2015-08-13 20:20:26 +0000

    install devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Chowscout::Application.configure do
+  config.action_mailer.default_url_options = { :host => 'chowscout.herokuapp.com' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
