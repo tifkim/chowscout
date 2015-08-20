305da6e9756f0cca84fd19e73fff2c0065b083bc
Tiff <kim.tiffany.min@gmail.com>
2015-08-20 19:35:04 +0000

    update edit form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -22,6 +22,11 @@ before_action :authenticate_user!, :only => [:new, :create]
     @place = Place.find(params[:id])
   end
 
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
 
 private
 
